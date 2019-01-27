6f80075777381d7afbfff067b8eadeb8020e4964
Anna Ross <anna.ross@chasacademy.se>
2019-01-27 14:59:41 +0100

    chore: Add mongoose db-connection

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const app = express();
-const port = 3001;
+const port = process.env.SERVER_PORT || 3001;
+
+// Connect to database
+mongoose
+  .connect(
+    process.env.DB_CONNECTION_URI,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log(`Successfully connected to database`))
+  .catch((error) => console.log(`Failed to connect to database, ${error}`));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
